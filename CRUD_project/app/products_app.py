d64f2fa8903b031e4a160d34ad6949402e30e3c8
Adrian Dahlin <akd361@nyu.edu>
2017-07-19 19:27:17 -0400

    1.3 and 1.4 handler function prints the name of the chosen operation and recognizes invalid operation inputs

--- CRUD_project/app/products_app.py
+++ CRUD_project/app/products_app.py
@@ -4,6 +4,7 @@ HELLO. YOU'RE THE BEST.
 There are 1,000,000 products.
 
 Please pick one of these five operations that you may conduct on these products:
+(case sensitive)
 - List
 - Show
 - Create
@@ -11,6 +12,42 @@ Please pick one of these five operations that you may conduct on these products:
 - Destroy
 ''')
 
+operations = ["List", "list", "Show", "show", "Create", "create", "Update", "update", "Destroy", "destroy"]
+
 inp = input("Type operation here:")
 
-print(inp)
+def list_product(inp):
+    print("You have selected the", inp, "operation.")
+
+def show_product(inp):
+    print("You have selected the", inp, "operation.")
+
+def create_product(inp):
+    print("You have selected the", inp, "operation.")
+    #name = input("Enter the product's name:")
+    #department = input("Enter the department the product is in:")
+    #aisle = input("Enter the aisle where the product sits:")
+    #price = input("Enter the product's price:")
+    #ident
+
+def update_product(inp):
+    print("You have selected the", inp, "operation.")
+
+def destroy_product(inp):
+    print("You have selected the", inp, "operation.")
+
+def handler(inp):
+    if inp == "List":
+        print(list_product(inp))
+    if inp == "Show":
+        print(show_product(inp))
+    if inp == "Create":
+        create_product(inp)
+    if inp == "Update":
+        print(update_product(inp))
+    if inp == "Destroy":
+        print(destroy_product(inp))
+    if inp not in operations:
+        print("Unrecognized Operation. Please choose one of: 'List', 'Show', 'Create', 'Update', or 'Destroy'.")
+
+print(handler(inp))
