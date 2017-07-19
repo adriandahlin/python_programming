9b5efc586fb33401575e6e18af8fd8926d1d696a
Adrian Dahlin <akd361@nyu.edu>
2017-07-19 18:48:35 -0400

    1.1 print a menu

--- CRUD_project/app/products_app.py
+++ CRUD_project/app/products_app.py
@@ -1,1 +1,14 @@
-print("HELLO")
+print('''
+HELLO. YOU'RE THE BEST.
+
+There are 1,000,000 products.
+
+There are five operations you may conduct on these products:
+- List
+- Show
+- Create
+- Update
+- Destroy
+
+HAVE A NICE DAY.
+''')
