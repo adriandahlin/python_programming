eeadcc3d4b9c1ac89338c6dba5eb869afde553cd
Adrian Dahlin <akd361@nyu.edu>
2017-07-19 20:01:58 -0400

    2.R.4 print menu with actual number of items in inventory

--- CRUD_project/app/products_app.py
+++ CRUD_project/app/products_app.py
@@ -1,59 +1,5 @@
 import csv
 
-# print('''
-# HELLO. YOU'RE THE BEST.
-#
-# There are 1,000,000 products.
-#
-# Please pick one of these five operations that you may conduct on these products:
-# (case sensitive)
-# - List
-# - Show
-# - Create
-# - Update
-# - Destroy
-# ''')
-
-operations = ["List", "list", "Show", "show", "Create", "create", "Update", "update", "Destroy", "destroy"]
-
-#inp = input("Type operation here:")
-
-def list_products():
-    print("You have selected the", inp, "operation.")
-
-def show_product():
-    print("You have selected the", inp, "operation.")
-
-def create_product():
-    print("You have selected the", inp, "operation.")
-    #name = input("Enter the product's name:")
-    #department = input("Enter the department the product is in:")
-    #aisle = input("Enter the aisle where the product sits:")
-    #price = input("Enter the product's price:")
-    #ident
-
-def update_product():
-    print("You have selected the", inp, "operation.")
-
-def destroy_product():
-    print("You have selected the", inp, "operation.")
-
-def handler(inp):
-    if inp == "List":
-        print(list_product(inp))
-    if inp == "Show":
-        print(show_product(inp))
-    if inp == "Create":
-        create_product(inp)
-    if inp == "Update":
-        print(update_product(inp))
-    if inp == "Destroy":
-        print(destroy_product(inp))
-    if inp not in operations:
-        print("Unrecognized Operation. Please choose one of: 'List', 'Show', 'Create', 'Update', or 'Destroy'.")
-
-#print(handler(inp))
-
 csv_file_path = "data/products.csv"
 
 rows = []
@@ -62,4 +8,58 @@ with open(csv_file_path, "r") as csv_file:
     for row in reader:
         rows.append(row)
 
-print(len(rows))
+#print(len(rows))
+
+menu = '''
+HELLO. YOU'RE THE BEST.
+
+There are {0} products.
+
+Please pick one of these five operations that you may conduct on these products:
+(case sensitive)
+- List
+- Show
+- Create
+- Update
+- Destroy
+'''.format(len(rows))
+
+print(menu)
+
+# operations = ["List", "list", "Show", "show", "Create", "create", "Update", "update", "Destroy", "destroy"]
+
+# def list_products():
+#     print("You have selected the", inp, "operation.")
+#
+# def show_product():
+#     print("You have selected the", inp, "operation.")
+#
+# def create_product():
+#     print("You have selected the", inp, "operation.")
+#     #name = input("Enter the product's name:")
+#     #department = input("Enter the department the product is in:")
+#     #aisle = input("Enter the aisle where the product sits:")
+#     #price = input("Enter the product's price:")
+#     #ident
+#
+# def update_product():
+#     print("You have selected the", inp, "operation.")
+#
+# def destroy_product():
+#     print("You have selected the", inp, "operation.")
+#
+# def handler():
+#     if inp == "List":
+#         print(list_product(inp))
+#     if inp == "Show":
+#         print(show_product(inp))
+#     if inp == "Create":
+#         create_product(inp)
+#     if inp == "Update":
+#         print(update_product(inp))
+#     if inp == "Destroy":
+#         print(destroy_product(inp))
+#     if inp not in operations:
+#         print("Unrecognized Operation. Please choose one of: 'List', 'Show', 'Create', 'Update', or 'Destroy'.")
+#
+# print(handler(inp))
