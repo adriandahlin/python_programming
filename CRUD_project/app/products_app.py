a175183cdb8e474be2d08015c970485e7e7deea5
Adrian Dahlin <akd361@nyu.edu>
2017-07-19 21:54:13 -0400

    implement create operation

--- CRUD_project/app/products_app.py
+++ CRUD_project/app/products_app.py
@@ -1,23 +1,9 @@
 import csv
 
-# #print(len(rows))
-#
-# menu = '''
-# HELLO. YOU'RE THE BEST.
-#
-# There are {0} products.
-#
-# Please pick one of these five operations that you may conduct on these products:
-# (case sensitive)
-# - List
-# - Show
-# - Create
-# - Update
-# - Destroy
-# '''.format(len(rows))
+
 
 csv_file_path = "data/products.csv"
-writing_path = "data/writing_products.csv"
+#writing_path = "data/writing_products.csv"
 
 rows = []
 
@@ -26,52 +12,74 @@ with open(csv_file_path, "r") as csv_file:
     for row in reader:
         rows.append(row)
 
-with open(csv_file_path, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle", "department", "price"])
-    writer.writeheader()
-    for row in rows:
-        writer.writerow(row)
-    writer.writerow({"id": "21", "name": "Thing 1", "aisle": "babies with attitude", "department": "babies", "price": "0.00"})
+menu = '''
+HELLO. YOU'RE THE BEST.
 
-# with open(other_path, "w") as csv_file:
-#     writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle", "department", "price"])
-#     writer.writeheader() # uses fieldnames set above
-#     writer.writerow({"id": "1", "name": "Thing 1", "aisle": "7", "department": "shit", "price": "0.00"})
+There are {0} products.
 
-# operations = ["List", "list", "Show", "show", "Create", "create", "Update", "update", "Destroy", "destroy"]
+Please pick one of these five operations that you may conduct on these products:
+(case sensitive)
+- List
+- Show
+- Create
+- Update
+- Destroy
+'''.format(len(rows))
 
-# def list_products():
-#     print("You have selected the", inp, "operation.")
-#
-# def show_product():
-#     print("You have selected the", inp, "operation.")
-#
-# def create_product():
-#     print("You have selected the", inp, "operation.")
-#     #name = input("Enter the product's name:")
-#     #department = input("Enter the department the product is in:")
-#     #aisle = input("Enter the aisle where the product sits:")
-#     #price = input("Enter the product's price:")
-#     #ident
-#
-# def update_product():
-#     print("You have selected the", inp, "operation.")
-#
-# def destroy_product():
-#     print("You have selected the", inp, "operation.")
-#
-# def handler():
-#     if inp == "List":
-#         print(list_product(inp))
-#     if inp == "Show":
-#         print(show_product(inp))
-#     if inp == "Create":
-#         create_product(inp)
-#     if inp == "Update":
-#         print(update_product(inp))
-#     if inp == "Destroy":
-#         print(destroy_product(inp))
-#     if inp not in operations:
-#         print("Unrecognized Operation. Please choose one of: 'List', 'Show', 'Create', 'Update', or 'Destroy'.")
-#
-# print(handler(inp))
+operations = ["List", "list", "Show", "show", "Create", "create", "Update", "update", "Destroy", "destroy"]
+
+inp = input(menu)
+
+def list_products():
+    print("You are listing a product or products.")
+
+def show_product():
+    print("You are showing a product.")
+
+def create_product():
+    print("creating a product")
+#    product_id = input("Enter the product's id:")
+    product_name = input("Enter the product's name:")
+    product_aisle = input("Enter the aisle where the product sits:")
+    product_department = input("Enter the department the product is in:")
+    product_price = input("Enter the product's price:")
+    new_product = {
+        "id": len(rows) + 1,
+        "name": product_name.title(),
+        "aisle": product_aisle,
+        "department": product_department,
+        "price": product_price,
+    }
+    with open(csv_file_path, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle", "department", "price"])
+        writer.writeheader()
+        for row in rows:
+            writer.writerow(row)
+        writer.writerow(new_product)
+
+def update_product():
+    print("You are updating a product.")
+
+def destroy_product():
+    print("You are destroying a product.")
+
+def handler():
+    if inp == "List":
+        print(list_products())
+    if inp == "Show":
+        print(show_product())
+    if inp == "Create":
+        create_product()
+    if inp == "Update":
+        print(update_product())
+    if inp == "Destroy":
+        print(destroy_product())
+    if inp not in operations:
+        print("Unrecognized Operation. Please choose one of: 'List', 'Show', 'Create', 'Update', or 'Destroy'.")
+
+print(handler())
+
+with open(csv_file_path, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        print(row["id"], row["name"], row["aisle"], row["department"], row["price"])
